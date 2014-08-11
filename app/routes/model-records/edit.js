c140d68e91bca98faf2a8c8d18202df1bf00b37f
Brian Cardarella <bcardarella@gmail.com>
2014-08-10 23:14:01 -0400

    Transition confirmations

--- app/routes/model-records/edit.js
+++ app/routes/model-records/edit.js
@@ -3,6 +3,6 @@ import EmberAdminRouteModelRecordsEditMixin from 'ember-admin/mixins/routes/mode
 import EmberDataRouteMixin from 'ember-data-route/mixins/data-route';
 
 export default Ember.Route.extend(
-  EmberAdminRouteModelRecordsEditMixin,
-  EmberDataRouteMixin
+  EmberDataRouteMixin,
+  EmberAdminRouteModelRecordsEditMixin
 );
