23d1e6012f39405586833c584e6e00ec8ac9e4f6
Brian Cardarella <bcardarella@gmail.com>
2015-03-06 19:11:45 -0500

    Reproduce

--- tests/helpers/equality-helpers.js
+++ tests/helpers/equality-helpers.js
@@ -1,28 +1,28 @@
 export { rowValuesEqual, inputPropertiesEqual };
 
-function rowValuesEqual () {
+function rowValuesEqual (assert) {
   var row = arguments[0];
   var values = Array.prototype.slice.call(arguments, 1, arguments.length);
   var columns = row.find('th, td');
   var columnText;
 
-  equal(columns.length, values.length, 'expected ' + values.length + ' columns: (' + values.join(', ') + ')');
+  assert.equal(columns.length, values.length, 'expected ' + values.length + ' columns: (' + values.join(', ') + ')');
 
   for (var i = 0; i < columns.length; i++) {
     columnText = columns.eq(i).text().trim();
-    equal(columnText, values[i], 'expected column(' + (i + 1) + ') with value: ' + values[i]);
+    assert.equal(columnText, values[i], 'expected column(' + (i + 1) + ') with value: ' + values[i]);
   }
 }
 
-function inputPropertiesEqual() {
+function inputPropertiesEqual(assert) {
   var inputs = arguments[0];
   var values = Array.prototype.slice.call(arguments, 1, arguments.length);
   var labelText;
 
-  equal(inputs.length, values.length, 'expected ' + values.length + ' inputs: (' + values.join(', ') + ')');
+  assert.equal(inputs.length, values.length, 'expected ' + values.length + ' inputs: (' + values.join(', ') + ')');
 
   for (var i = 0; i < inputs.length; i ++) {
     labelText = inputs.eq(i).parent().text().trim();
-    equal(labelText, values[i], 'expected input(' + (i + 1) + ') for property: ' + values[i]);
+    assert.equal(labelText, values[i], 'expected input(' + (i + 1) + ') for property: ' + values[i]);
   }
 }
