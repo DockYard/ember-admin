40e31ff90eddba9ddd61aa8c985ad76f356d5695
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-07-13 22:37:55 -0300

    Use shouldBackgroundReloadRecord in application adapter

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,3 +1,7 @@
 import DS from 'ember-data';
 
-export default  DS.RESTAdapter.extend({});
+export default  DS.RESTAdapter.extend({
+  shouldBackgroundReloadRecord: function(){
+  	return true;
+  }
+});
