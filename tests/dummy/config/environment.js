9f341ca588ddb3ff5c5c50a1fb124357cb88d2ed
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-07-13 22:18:16 -0300

    Install ember-cli-filter

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -7,6 +7,7 @@ module.exports = function(environment) {
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
+      ENABLE_DS_FILTER: true,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
