94f2d30f5f79578c4eee3bb8cd7e9fa6d4a6ed6c
Ilya Radchenko <ilya@burstcreations.com>
2016-10-25 16:21:09 -0400

    Use admin config

--- addon/routes/model-records/index.js
+++ addon/routes/model-records/index.js
@@ -2,12 +2,17 @@ import Ember from 'ember';
 
 const {
   get,
+  inject,
   Route
 } = Ember;
 
 export default Route.extend({
+  adminConfig: inject.service(),
+
   model() {
-    return this.admin.store.findAll(this.paramsFor('model-records').name).then(function(records) {
+    let config = this.get('adminConfig');
+    debugger;
+    return config.store.findAll(this.paramsFor('model-records').name).then(function(records) {
       return records.filter(function(item) {
         return !get(item, 'isNew');
       });
