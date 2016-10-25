856f64904f116a83090c8fb3ddd95b2c2db0da67
Ilya Radchenko <ilya@burstcreations.com>
2016-10-25 16:17:37 -0400

    Don't reset namespace

--- addon/routes.js
+++ addon/routes.js
@@ -1,7 +1,7 @@
 import buildRoutes from 'ember-engines/routes';
 
 export default buildRoutes(function() {
-  this.route('model-records', { path: ':name', resetNamespace: true }, function() {
+  this.route('model-records', { path: ':name' }, function() {
     this.route('edit', { path: ':id/edit' });
     this.route('new');
   });
