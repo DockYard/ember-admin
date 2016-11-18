8795682c2d1ee3a352fe6f2fa8957fdc02766e29
Ilya Radchenko <ilya@burstcreations.com>
2016-11-17 21:23:37 -0500

    Move all templates back

--- addon/mixins/model-records/write.js
+++ addon/mixins/model-records/write.js
@@ -3,21 +3,10 @@ import EmberDataRouteMixin from 'ember-data-route';
 
 const {
   get,
-  getOwner,
   Mixin
 } = Ember;
 
 export default Mixin.create(EmberDataRouteMixin, {
-  renderTemplate() {
-    let templatePath = `${this.templateAdminPath}/${this.paramsFor('model-records').name}`;
-    let defaultTemplatePath = `${this.templateAdminPath}/default`;
-
-    if (getOwner(this).lookup(`template:${templatePath}`)) {
-      this.render(templatePath);
-    } else {
-      this.render(defaultTemplatePath);
-    }
-  },
   actions: {
     save(callback) {
       let promise = get(this, 'controller.model').save();
